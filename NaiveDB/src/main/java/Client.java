c92f56a285dfbc5c6de881d21f373b8a6327ed99
liuyijian <liuyijian16@163.com>
2019-04-13 00:48:21 +0800

    gradle项目搭建 && 完成通信模块

---
+++ NaiveDB/src/main/java/Client.java
@@ -0,0 +1,70 @@
+import org.apache.commons.io.FileUtils;
+
+import java.io.*;
+import java.net.ConnectException;
+import java.net.Socket;
+
+public class Client {
+    /*
+        Server运行后，允许创建连接，后面的可选（Server没运行时，报错或阻塞）
+        循环读取SQL命令并发给Server，直至退出
+     */
+    public static final int PORT = 12306;
+    public static final String IP_ADDR = "localhost";
+
+    public static void main(String[] args){
+
+        try (
+                Socket socket = new Socket(IP_ADDR, PORT);
+                DataInputStream input = new DataInputStream(socket.getInputStream());
+                DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+                ){
+            System.out.println("naiveDB client is running!");
+
+            while(true) {
+                String outputStr = bufferedReader.readLine();
+                if (outputStr.startsWith("import")){
+
+                    String file_name = outputStr.split("\\s+")[1];
+                    // 此处用了第三方jar包org.apache.commons.io.FileUtil
+                    // 参考http://commons.apache.org/proper/commons-io/javadocs/api-2.6/index.html
+                    try {
+                        outputStr = FileUtils.readFileToString(new File(file_name), "UTF-8");
+                    } catch (IOException e){
+                        System.out.println("找不到txt文件");
+                        continue;
+                    }
+                    // 目前的方法是一股脑丢过去，考虑分句丢过去？
+                    out.writeUTF(outputStr);
+                    long startTime = System.nanoTime();
+                    String inputStr = input.readUTF();
+                    long endTime = System.nanoTime();
+                    long costTime = (endTime-startTime)/1000;
+                    System.out.println("用时（毫秒）：\n"+costTime);
+                    System.out.println("返回结果:\n" + inputStr);
+                    if (inputStr.endsWith("Bye")) {
+                        System.out.println("客户端将关闭连接");
+                        throw new IOException("客户端主动退出");
+                    }
+                }
+                else {
+                    out.writeUTF(outputStr);
+                    String inputStr = input.readUTF();
+                    System.out.println("返回结果:\n" + inputStr);
+                    if (inputStr.endsWith("Bye")) {
+                        System.out.println("客户端将关闭连接");
+                        throw new IOException("客户端主动退出");
+                    }
+                }
+            }
+        } catch (ConnectException e) {
+            System.out.println("无法连接到服务器（忘了打开服务器了吗？）");
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+}
---
+++ NaiveDB/src/main/java/Server.java
@@ -0,0 +1,95 @@
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.List;
+
+public class Server {
+    /*
+
+        加载恢复元数据信息，
+        接受client连接
+        循环接受SQL命令，直至exit退出，销毁client的socket
+        把命令发给SQLParser类处理，不同命令对应不同的操作（写在Parser里还是写个util类？）
+     */
+    // 服务器监听端口
+    public static final int PORT = 12306;
+
+    // （可选）维护一个客户端连接的socket列表，如果要做的话，记得客户退出后，把列表更新
+    // List<Socket> socketList;
+
+    // 加载一个MetaData类的对象
+
+    //初始化服务器
+    public void init(){
+        // 出错的话会自动回收socket
+
+        // 此处应该先加载元数据
+
+        try(ServerSocket serverSocket = new ServerSocket(PORT)){
+            System.out.println("naiveDB server is running!");
+            while(true){
+                // 阻塞接受客户端连接
+                Socket client = serverSocket.accept();
+                // 为客户端新建线程
+                new HandlerThread(client);
+            }
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+
+    //维护客户端线程的内部类
+    private class HandlerThread implements Runnable {
+
+        public Socket socket;
+
+        public HandlerThread(Socket socket){
+            this.socket = socket;
+            new Thread(this).start();
+        }
+
+        @Override
+        public void run(){
+            try (
+                    // 新建数据输入输出流对象，写入try的资源块中
+                    DataInputStream input = new DataInputStream(socket.getInputStream());
+                    DataOutputStream out = new DataOutputStream(socket.getOutputStream());
+                    ){
+
+                while(true){
+                    // 此处未来可扩展的功能：权限认证，客户端quit命令，等等
+
+                    //读取客户端数据
+                    String inputStr = input.readUTF();
+                    System.out.println("客户端发来数据是：\n" + inputStr);
+                    //向客户端发送数据，为调用SQLParser类的犯法处理输入后返回的结果
+                    String outputStr = inputStr;
+                    out.writeUTF(outputStr);
+                }
+            } catch (IOException e){
+                System.out.println("服务器异常" + e.getMessage());
+            } catch (Exception e) {
+                // 这里修改为自定义的Exception，来对应client的quit命令，SQLParser中处理到quit就去throw一个出来;
+                System.out.println("客户端终止了连接" + e.getMessage());
+            } finally {
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (Exception e){
+                        socket = null;
+                        System.out.println("服务器finally异常" + e.getMessage());
+                    }
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args){
+        Server server = new Server();
+        server.init();
+    }
+}
+
+
